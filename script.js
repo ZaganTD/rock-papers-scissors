740faf8fc3becac7003cc85b854163878e791fb5
ZaganTD <203254093+ZaganTD@users.noreply.github.com>
2025-07-11 12:07:41 -0700

    Done the human choice function

--- script.js
+++ script.js
@@ -1,83 +1,8 @@
-function humanChoice () {
+function humanChoice() {
     let x = prompt('Choose your weapon:')
-    x = x.toLowerCase()
-    console.log(x)
-    switch (x) {
-        case 'rock':
-            return 'rock';
-            break
-        case 'paper':
-            return 'paper';
-            break;
-        case 'scissor':
-            return 'scissor';
-            break;
-        default:
-            alert('Wrong input!')
-    }   
-
+    x = x.toLowerCase();
+    if (x == 'rock' || x == 'paper' || x == 'scissor') {
+        return x 
+    } else alert('Wrong input!')
 }
 
-function computerChoice() {
-    let r = Math.ceil(Math.random() * 10)
-    if (r > 3.33 && r <= 6.66 && 0) {
-        return 'paper'
-    } else if (r <= 3.33) {
-        return 'rock'
-    } else if (r > 6.66) {
-        return 'scissor'
-    }
-}
-
-let hChoice = humanChoice()
-let cChoice = computerChoice()
-
-function humanWins() {
-    if (hChoice == 'paper' && cChoice == 'rock') {
-        return true
-    } else if (hChoice == 'scissor' && cChoice == 'paper') {
-        return true
-    } else if (hChoice == 'rock' && cChoice == 'scissor') {
-        return true
-    } else return false
-}
-
-function computerWins() {
-    if (hChoice == 'rock' && cChoice == 'paper') {
-        return true
-    } else if (hChoice == 'scissor' && cChoice == 'rock') {
-        return true
-    } else if (hChoice == 'paper' && cChoice == 'scissor') {
-        return true
-    } else return false
-}
-
-let humanScore = 0
-let computerScore = 0
-
-
-function playRound () {
-    humanChoice()
-    computerChoice()
-    if (cChoice == undefined | hChoice == undefined) {
-        console.log('Error! You have not chosen anything!')
-    } else if (hChoice == cChoice) {
-        console.log(`${hChoice} and ${cChoice} are equal! It\'s a Tie!`)
-    } else if (humanWins()) {
-        console.log(`${hChoice} beats ${cChoice}! You win!`)
-        ++humanScore
-        console.log(humanScore)
-        console.log(computerScore)
-    } else if (computerWins) {
-        console.log(`${cChoice} beats ${hChoice}! You lose!`)
-        ++computerScore
-        console.log(humanScore)
-        console.log(computerScore)
-    } 
-}
-
-playRound();
-playRound();
-playRound();
-playRound();
-playRound();
