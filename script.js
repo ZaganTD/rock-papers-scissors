6c858f12e33b4603eb74005aca7d932ff83cf519
ZaganTD <203254093+ZaganTD@users.noreply.github.com>
2025-07-25 03:24:38 -0700

    Done

--- script.js
+++ script.js
@@ -1,60 +1,72 @@
+const button = document.querySelectorAll('.button')
+const announcement = document.querySelector('.anoun')
+let scorePc = document.querySelector('.score-pc')
+let scoreH = document.querySelector('.score-human')
+
+announcement.style.fontSize = '50px'
+announcement.style.fontFamily = 'bold'
+
+let comChoice
+let humChoice
+let computerScore = 0
+let humanScore = 0
 
 function computerChoice() {
-    let y = Math.random() * 10
-    if (y <= 3.33) {
-        return 'Rock'
-    } else if (y > 3.33 && y <= 6.66) {
-        return 'Paper'
-    } else return 'Scissor'
+    const array = ['Rock', 'Paper', 'Scissor']
+    comChoice = array[Math.floor(Math.random() * array.length)]
 }
 
 
-function humanWin(human, computer) {
-    if (human == 'Rock' && computer == 'Scissor') {
+button.forEach((i) => {
+    i.addEventListener('click', (i) => humChoice = i.target.innerHTML)
+})
+
+button.forEach((i) => {
+    i.addEventListener('click', () => playRound())
+}) 
+
+function humanWin() {
+    if (humChoice == 'Rock' && comChoice == 'Scissor') {
         return true
-    } else if (human == 'Paper' && computer == 'Rock') {
+    } else if (humChoice == 'Scissor' && comChoice == 'Paper') {
         return true
-    } else if (human == 'Scissor' && computer == 'Paper') {
+    } else if (humChoice == 'Paper' && comChoice == 'Rock') {
         return true
     } else return false
 }
 
-function computerWin(computer, human) {
-    if (computer == 'Rock' && human == 'Scissor') {
+function computerWin() {
+    if (humChoice == 'Scissor' && comChoice == 'Rock') {
         return true
-    } else if (computer == 'Paper' && human == 'Rock') {
+    } else if (humChoice == 'Paper' && comChoice == 'Scissor') {
         return true
-    } else if (computer == 'Scissor' && human == 'Paper') {
+    } else if (humChoice == 'Rock' && comChoice == 'Paper') {
         return true
     } else return false
 }
 
-let humanScore = document.querySelector('.score-human')
-let pcScore = document.querySelector('.score-pc')
-let button = document.querySelectorAll('.button')
-let container = document.querySelector('.container')
-let anounContainer = document.querySelector('.anoun')
+function playRound() {
+    computerChoice()
+    if (humChoice == comChoice) {
+        announcement.textContent = `It's a tie!`
+    } else if (humanWin()) {
+        humanScore++
+        scoreH.textContent = humanScore
+        announcement.textContent = `You win! ${humChoice} beats ${comChoice}!`
+    } else if (computerWin()) {
+        computerScore++
+        scorePc.textContent = computerScore
+        announcement.textContent = `You lose! ${comChoice} beats ${humChoice}!`
+    }
 
-
-for (let i of button) {
-    i.addEventListener('click', (e) => {
-        if (e.target.innerHTML === computerChoice()) {
-            anounContainer.innerHTML = ''
-            let anoun = document.createElement('h1')
-            anoun.textContent = `It's a tie! ${e.target.innerHTML} and ${computerChoice()} are equal`
-            anounContainer.appendChild(anoun)
-        } else if (humanWin(e.target.innerHTML, computerChoice())) {
-            anounContainer.innerHTML = ''
-            let anoun = document.createElement('h1')
-            anoun.textContent = `You win! ${e.target.innerHTML} beats ${computerChoice()}`
-            anounContainer.appendChild(anoun)
-        } else if (computerWin(computerChoice(), e.target.innerHTML)) {
-            anounContainer.innerHTML = ''
-            let anoun = document.createElement('h1')
-            anoun.textContent = `You lose! ${computerChoice()} beats ${e.target.innerHTML}`
-            anounContainer.appendChild(anoun)
-        }
-
-    })
+    gameOver()
 }
 
+
+function gameOver() {
+    if (humanScore == 5) {
+        announcement.textContent = `The game is over! You win!`
+    } else if (computerScore == 5) {
+        announcement.textContent = `The game is over! You lose!`
+    }
+}
